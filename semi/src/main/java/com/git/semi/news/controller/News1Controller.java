1d38bc2080c45b94a66b2f94a141553c81a17210
goss <goss1997@naver.com>
2024-07-26 10:42:10 +0900

    순철이형님과 깃 에러를 방지하기 위해 mvc 2개로 우선 작업.
    순쳘이형님 = news ~~
    성수 = news1 ~~
    에서 작업할 것.

---
+++ semi/src/main/java/com/git/semi/news/controller/News1Controller.java
@@ -0,0 +1,7 @@
+package com.git.semi.news.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class News1Controller {
+}
---
+++ semi/src/main/java/com/git/semi/news/dao/News1Dao.java
@@ -0,0 +1,8 @@
+package com.git.semi.news.dao;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class News1Dao {
+}
